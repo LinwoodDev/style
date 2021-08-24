820b72e20ccb2667fdc95b54a65aebd22192f408
CodeDoctor <wusel905@mail.com>
2021-08-24 17:00:25 +0200

    init

---
+++ src/components/Button.md
@@ -0,0 +1,6 @@
+A very simple button.
+
+```jsx
+import Button from "./Button";
+<Button text="We Salute You!" />
+```
---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Button({text} : {text: string }) {
+  return <button>{text}</button>;
+}
