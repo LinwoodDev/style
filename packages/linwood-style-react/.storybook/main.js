b60f5d8ccf78e1f15863ef0249454d2061a7e61b
CodeDoctorDE <codedoctor@linwood.dev>
2022-01-10 06:24:40 +0100

    add monorepo, add sass support

--- packages/linwood-style-react/.storybook/main.js
+++ packages/linwood-style-react/.storybook/main.js
@@ -1,33 +1,14 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.tsx"
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
   ],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(scss|sass)$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../")
-    });
-
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve("babel-loader"),
-      options: {
-        presets: [
-          ["react-app", {
-            flow: false,
-            typescript: true
-          }]
-        ]
-      }
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
-
-    return config;
-  },
-  "framework": "@storybook/react"
+  "core": {
+    "builder": "webpack5"
+  }
 }
