10b9e9a435c2db9bef54a4e8822cf1ff69439a47
CodeDoctorDE <codedoctor@linwood.dev>
2022-02-08 09:05:23 +0100

    add new touchable style

--- packages/linwood-style-web/src/components/Button/Button.tsx
+++ packages/linwood-style-web/src/components/Button/Button.tsx
@@ -1,46 +1,20 @@
-import React, { MouseEventHandler, ReactElement, useState } from 'react'
+import React, { ReactElement, useState } from 'react'
 import Touchable from '../Touchable/Touchable';
 import './Button.sass'
+import { TouchableSize, TouchableVariant } from '../Touchable/Touchable';
 
 export interface ButtonProps {
     label: string,
-    size: 'small' | 'medium' | 'large',
+    size?: TouchableSize,
+    variant?: TouchableVariant
 }
 
-export default function Button({ label, size }: ButtonProps): ReactElement {
-    const buttonLightRef = React.createRef<HTMLSpanElement>()
-    const [full, setFull] = useState(false);
-
-    const moveLight = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
-        const button = event.currentTarget;
-        const rect = button.getBoundingClientRect();
-
-        const circle = buttonLightRef.current!;
-        const diameter = Math.max(button.clientWidth, button.clientHeight);
-        const radius = diameter / 2;
-        var zoom = (document.body.style as any).zoom as number || 1;
-        var x = (event.clientX - rect.left - radius) / zoom;
-        var y = (event.clientY - rect.top - radius) / zoom;
-        circle.style.left = `${x}px`;
-        circle.style.top = `${y}px`;
-
-        circle.style.width = circle.style.height = `${diameter / zoom}px`;
-        circle.classList.add("button-light");
-    };
-    const click = () => {
-        setFull(false)
-        setTimeout(() => {
-            setFull(true)
-        }, 100)
-    }
-    const leave = () => {
-        setFull(false)
-    }
+export default function Button({ label, size, variant }: ButtonProps): ReactElement {
     return (
-        <Touchable onClick={click}>
+        <Touchable variant={variant}>
             <div className={`button button--${size}`}>
                 {label}
             </div>
-        </Touchable>
+        </Touchable >
     )
 }
--- packages/linwood-style-web/src/components/Touchable/Touchable.tsx
+++ packages/linwood-style-web/src/components/Touchable/Touchable.tsx
@@ -1,43 +1,17 @@
-import React, { MouseEventHandler, PropsWithChildren, ReactElement, useState } from 'react'
+import React, { MouseEventHandler, PropsWithChildren, ReactElement } from 'react'
 import './Touchable.sass'
+export type TouchableSize = 'small' | 'medium' | 'large';
+export type TouchableVariant = 'filled' | 'normal' | 'outlined';
 
 export interface TouchableProps {
-    onClick: MouseEventHandler<HTMLButtonElement>
+    onClick?: MouseEventHandler,
+    variant?: TouchableVariant
 }
 
-export default function Touchable({ children }: PropsWithChildren<TouchableProps>): ReactElement {
-    const touchableLightRef = React.createRef<HTMLSpanElement>()
-    const [full, setFull] = useState(false);
-
-    const moveLight = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
-        const touchable = event.currentTarget;
-        const rect = touchable.getBoundingClientRect();
-
-        const circle = touchableLightRef.current!;
-        const diameter = Math.max(touchable.clientWidth, touchable.clientHeight);
-        const radius = diameter / 2;
-        var zoom = (document.body.style as any).zoom as number || 1;
-        var x = (event.clientX - rect.left - radius) / zoom;
-        var y = (event.clientY - rect.top - radius) / zoom;
-        circle.style.left = `${x}px`;
-        circle.style.top = `${y}px`;
-
-        circle.style.width = circle.style.height = `${diameter / zoom}px`;
-        circle.classList.add("Touchable-light");
-    };
-    const click = () => {
-        setFull(false)
-        setTimeout(() => {
-            setFull(true)
-        }, 100)
-    }
-    const leave = () => {
-        setFull(false)
-    }
+export default function Touchable({ children, variant, onClick }: PropsWithChildren<TouchableProps>): ReactElement {
     return (
-        <div className={`touchable`} onMouseMove={moveLight} onClick={click} onMouseOut={leave}>
-            <span className={"touchable-light" + (full ? " touchable-light-full" : "")} ref={touchableLightRef} />
-            <div>{children}</div>
-        </div>
+        <div onClick={onClick} className={`touchable touchable--${variant ?? 'normal'}`}>
+            {children}
+        </div >
     )
 }
