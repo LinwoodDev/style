5542cf49990772d2223d8098f79162fe89743867
CodeDoctorDE <codedoctor@linwood.dev>
2024-02-15 20:53:44 +0100

    Update project

--- docs/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import LinwoodStyleIntegration from 'linwood-style-astro';
-
-// https://astro.build/config
-export default defineConfig({
-    integrations: [
-        LinwoodStyleIntegration(),
-    ]
-});
